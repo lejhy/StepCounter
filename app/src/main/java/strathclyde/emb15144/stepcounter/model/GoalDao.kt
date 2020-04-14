f1ce2952f5fdab1c1b53c321f4ce79581201ad3b
Jordan Howorth <howorthjz003@gmail.com>
2020-04-15 00:20:33 +0100

    Add convenience methods to GoalDao to simplify figuring out next-best goal.

--- app/src/main/java/strathclyde/emb15144/stepcounter/model/GoalDao.kt
+++ app/src/main/java/strathclyde/emb15144/stepcounter/model/GoalDao.kt
@@ -8,16 +8,28 @@ import androidx.room.*
 interface GoalDao {
     @Insert
     fun insert(goal: Goal)
+
     @Update
     fun update(goal: Goal)
+
     @Delete
     fun delete(goal: Goal)
+
     @Query("SELECT * FROM goals_table WHERE id = :key")
     fun get(key: Long): Goal?
+
     @Query("SELECT * FROM goals_table WHERE name = :name")
     fun get(name: String): Goal?
+
     @Query("SELECT * FROM goals_table")
     fun getAllObservable(): LiveData<List<Goal>>
+
     @Query("SELECT * FROM goals_table")
     fun getAllCursor(): Cursor
+
+    @Query("SELECT * FROM goals_table")
+    fun getAll(): List<Goal>
+
+    @Query("SELECT * FROM goals_table WHERE id = :key")
+    fun getCursor(key: Long): Cursor
 }
