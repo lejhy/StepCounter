b421ea56801f0b2b4db93ca679c9d4d24ced68ce
Jordan Howorth <howorthjz003@gmail.com>
2020-04-15 01:20:56 +0100

    Generify action handling on the framework's side by not expecting data in intent.

--- app/src/main/java/strathclyde/emb15144/stepcounter/receiver/ActionBroadcastReceiver.kt
+++ app/src/main/java/strathclyde/emb15144/stepcounter/receiver/ActionBroadcastReceiver.kt
@@ -6,49 +6,48 @@ import android.content.Intent
 import android.util.Log
 import android.widget.Toast
 import strathclyde.emb15144.stepcounter.model.MainDatabase
+import strathclyde.emb15144.stepcounter.provider.MainContentProvider
 import strathclyde.emb15144.stepcounter.ui.MainActivity
 
 class ActionBroadcastReceiver : BroadcastReceiver() {
 
+    private fun view(context: Context) {
+        context.startActivity(
+            Intent(
+                context,
+                MainActivity::class.java
+            ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        )
+        context.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
+    }
+
+    private fun changeGoal(context: Context){
+        val db = MainDatabase.getInstance(context)
+        val dayDao = db.dayDao
+
+        val newGoal = MainContentProvider.getClosestGoal(db)
+        val today = dayDao.getLatest()
+
+        val oldGoal = today.goal_name
+        today.goal_id = newGoal.id
+        today.goal_name = newGoal.name
+        today.goal_steps = newGoal.steps
+        dayDao.update(today)
+
+        Toast.makeText(
+            context,
+            "Changed the active goal from $oldGoal to ${today.goal_name}",
+            Toast.LENGTH_LONG
+        ).show()
+    }
     override fun onReceive(context: Context, intent: Intent) {
         when (intent.action) {
             "view" -> {
-                context.startActivity(
-                    Intent(
-                        context,
-                        MainActivity::class.java
-                    ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                )
-                context.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
+                view(context)
             }
             "Change Goal" -> {
-                val dayDao = MainDatabase.getInstance(context).dayDao
-                val goalDao = MainDatabase.getInstance(context).goalDao
-
-                val goalName = intent.extras?.getString("goalName")
-                if (goalName == null || goalName.isEmpty()) {
-                    Log.d("ChangeGoal", "Invalid Goal Name: $goalName")
-                    return
-                }
-
-                val today = dayDao.getLatest()
-                val newGoal = goalDao.get(goalName)
-
-                if (newGoal == null) {
-                    Log.d("ChangeGoal", "Could not find goal with name $goalName")
-                    return
-                }
-                val oldGoal = today.goal_name
-                today.goal_id = newGoal.id
-                today.goal_name = newGoal.name
-                today.goal_steps = newGoal.steps
-                dayDao.update(today)
-
-                Toast.makeText(
-                    context,
-                    "Changed the active goal from $oldGoal to ${today.goal_name}",
-                    Toast.LENGTH_LONG
-                ).show()
+                changeGoal(context)
+                view(context)
             }
             else -> {
                 Log.e(
